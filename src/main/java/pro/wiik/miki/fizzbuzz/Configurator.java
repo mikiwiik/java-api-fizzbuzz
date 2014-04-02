c38774b5e23edaaee95336d620b64158e2701bb8
Miki Wiik <miki.wiik@service-flow.com>
2014-04-02 22:43:57 +0300

    Move ContextConfiguration fom xml to java

---
+++ src/main/java/pro/wiik/miki/fizzbuzz/Configurator.java
@@ -0,0 +1,11 @@
+package pro.wiik.miki.fizzbuzz;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("pro.wiik.miki.fizzbuzz")
+@EnableWebMvc
+public class Configurator {
+}
--- src/test/java/pro/wiik/miki/fizzbuzz/RestApiTest.java
+++ src/test/java/pro/wiik/miki/fizzbuzz/RestApiTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+@ContextConfiguration(classes = Configurator.class)
 public class RestApiTest {
     private MockMvc mockMvc;
 
