bd795c69443c8d906e1cc0b4e995d1b8802651a5
Miki Wiik <miki.wiik@service-flow.com>
2016-01-22 21:41:04 +0200

    Handle NumberFormatException:s caused by non-numbers parameters as 400

--- src/main/java/pro/wiik/miki/fizzbuzz/RestApi.java
+++ src/main/java/pro/wiik/miki/fizzbuzz/RestApi.java
@@ -1,12 +1,12 @@
 package pro.wiik.miki.fizzbuzz;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.math.BigInteger;
 
 @Controller
@@ -21,5 +21,10 @@ public class RestApi {
     public String doFizzBuzz(@PathVariable("number") String number) {
         final BigInteger bigInt = new BigInteger(number);
         return fizzBuzzer.evaluate(bigInt);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    void handleIllegalArgumentException(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
     }
 }
