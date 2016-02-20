70efdbe95fe5a2ac0ec0adfc4127443318161f43
Miki Wiik <miki.wiik@service-flow.com>
2016-02-20 22:35:50 +0200

    Add minimal test scaffolding based on https://github.com/angular/angular-seed

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+    config.set({
+
+        basePath: './',
+
+        files: [
+            'src/test/js/**/*.js'
+        ],
+        autoWatch: true,
+
+        frameworks: ['jasmine'],
+
+        browsers: ['Chrome'],
+
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+        ],
+
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+
+    });
+};
---
+++ src/test/js/a_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Just testing', function () {
+
+    describe('Hello world', function () {
+
+        it('should ....', function () {
+            expect("Hello world").toEqual("Hello world");
+        })
+
+    });
+});
