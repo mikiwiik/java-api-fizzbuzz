58803d56bba4933682151753891fcc913912fc96
Miki Wiik <miki.wiik@service-flow.com>
2016-02-29 22:52:58 +0200

    Move grid defining div and style outside directive

--- src/main/webapp/js/common/Directives.js
+++ src/main/webapp/js/common/Directives.js
@@ -10,14 +10,12 @@ function LatestResults() {
         controller: 'ResultCtrl',
         controllerAs: 'vm',
         template: [
-            '<div class="col-md-6 col-md-offset-3">',
-            '  <div class="panel panel-default" style="margin-top: 20px">',
-            '    <div class="panel-heading">',
-            '      <h3 id="results-heading" class="panel-title">{{resultsHeading}}</h3>',
-            '    </div>',
-            '    <div class="panel-body" ng-repeat="test in vm.tests">',
-            '      <div>{{test.number}}:{{test.result}}</div>',
-            '    </div>',
+            '<div class="panel panel-default">',
+            '  <div class="panel-heading">',
+            '    <h3 id="results-heading" class="panel-title">{{resultsHeading}}</h3>',
+            '  </div>',
+            '  <div class="panel-body" ng-repeat="test in vm.tests">',
+            '    <div>{{test.number}}:{{test.result}}</div>',
             '  </div>',
             '</div>'].join('')
     }
