b9fa63488140d297d69c57fec3b064b68984bc2a
Miki Wiik <miki.wiik@service-flow.com>
2016-03-01 23:00:39 +0200

    Get rid of isolate scope for passing in headings so that the the same controller scope can be used across directives

--- src/main/webapp/js/common/Directives.js
+++ src/main/webapp/js/common/Directives.js
@@ -2,22 +2,22 @@
 
 function LatestResults() {
     return {
-        scope: {
-            resultsHeading: '@'
-        },
         restrict: 'E',
         replace: true,
         controller: 'ResultCtrl',
         controllerAs: 'vm',
-        template: [
-            '<div class="panel panel-default">',
-            '  <div class="panel-heading">',
-            '    <h3 id="results-heading" class="panel-title">{{resultsHeading}}</h3>',
-            '  </div>',
-            '  <div class="panel-body" ng-repeat="test in vm.tests">',
-            '    <div>{{test.number}}:{{test.result}}</div>',
-            '  </div>',
-            '</div>'].join('')
+        template: function(elem, attr) {
+            return [
+                '<div class="panel panel-default">',
+                '  <div class="panel-heading">',
+                '    <h3 id="results-heading" class="panel-title">' + attr.resultsHeading + '</h3>',
+                '  </div>',
+                '  <div class="panel-body" ng-repeat="test in vm.tests">',
+                '    <div>{{test.number}}:{{test.result}}</div>',
+                '  </div>',
+                '</div>'
+            ].join('')
+        }
     }
 }
 angular.module('fizzbuzz.directives', [])
